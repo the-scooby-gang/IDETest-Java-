9fac39f1112447f7cc6fd341fc87408687b3973c
Aimee Bunyan <aimeebunyan14091990@hotmail.co.uk>
2022-09-20 13:03:51 +0100

    test

---
+++ src/main/java/Runner.java
@@ -0,0 +1,12 @@
+public class Runner {
+
+    public static void main(String[] args) {
+        Bear bear = new Bear("Balu", 25, 95.62);
+        String name = bear.getName();
+        System.out.println(name);
+        bear.setName("Baloo");
+        System.out.println(bear.getName());
+    }
+
+
+}
